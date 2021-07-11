a59348854048df7d449a8b86d406d2ebe5469a6f
rawalprakash722 <rawalprakash722@gmail.com>
2021-07-11 11:11:18 +0545

    full login

---
+++ src/Container/Logout.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+
+class Logout extends Component{
+    componentDidMount(){
+        
+    }
+    render()
+    {
+        return(
+            <div>Unauth</div>
+        )
+    }
+   
+       
+    }
+
+
+export default Logout
