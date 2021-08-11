6a5a4741ad6c487f72276bda244ddb5718b9a270
parakramlama <tmngparakram@gmail.com>
2021-08-11 17:26:29 +0545

    registration edit

--- src/Container/Register.js
+++ src/Container/Register.js
@@ -21,62 +21,45 @@ class Register extends Component{
     render(){
         return(
 
-<section className="Form my-4 mx-5 pt-5 pb-5">
-            <div className="container">
-                <div className="row no-gutters shadow" style={{background:'white', borderRadius:'3px'}}>
+            <section className="Form my-4 mx-5 pt-5 pb-5">
+                <div className="container">
+                    <div className="row no-gutters shadow" style={{background:'white', borderRadius:'3px'}}>
                    
-                    <div className="col-lg-5">
-                      
+                        <div className="col-lg-5">
                             <img alt="login" src="https://www.gpslink.co.uk/static/assets/img/login.png" className="img-fluid" style={{ height: '400px',marginTop:'100px' }} />
+                        </div>
 
-                        
-                    </div>
-                    <div className="col-lg-5 no gutters">
-                       
-                    <h1 className="font-weight-bold py-4">Registration Form</h1>
-                    <h5 className="ml-5">Fill your registration form now</h5>
-        <br>
-        </br>
+                        <div className="col-lg-5 no gutters">
+                            <h1 className="font-weight-bold py-4">Registration Form</h1>
+                            <h5 className="ml-5">Fill your registration form now</h5>
+        
+                        <br>
+                        </br>
                              
 
+                            <form>
+        
+                                <div className="form-group"><input type="Text" className="form-control" placeholder="Name" value={this.state.fullname} onChange={(event)=>{this.setState({fullname:event.target.value})}}/>
+                                </div>
 
-                                <form>
-     
-         
+                                <div className="form-group"><input type="Text" className="form-control" placeholder="Email" value={this.state.email} onChange={(event)=>{this.setState({email:event.target.value})}}/>
+                                </div>
 
+                                <div className="form-group"><input type="Password" className="form-control" placeholder="Password" value={this.state.password} onChange={(event)=>{this.setState({password:event.target.value})}}/>
+                                </div>
+                                <div className="form-group"><input type="Password" className="form-control" placeholder="Confirm Password"/></div>
+                            
+                                <button  style={{textAlign:'center'}} type="submit" onClick={this.sendUserData} className="btn btn-primary"> Sign up </button>
 
+                                <br></br>
 
+                                <p className="signUp text-left ml-3"> Already have an account <Link exact to="login"><b> Login here </b></Link></p>
 
-
-            <div className="form-group"><input type="Text" className="form-control" placeholder="Name" value={this.state.fullname} onChange={(event)=>{this.setState({fullname:event.target.value})}}/>
-           </div>
-
-           <div className="form-group"><input type="Text" className="form-control" placeholder="Email" value={this.state.email} onChange={(event)=>{this.setState({email:event.target.value})}}/>
-           </div>
-
-          
-           <div className="form-group"><input type="Password" className="form-control" placeholder="Password" value={this.state.password} onChange={(event)=>{this.setState({password:event.target.value})}}/>
-           </div>
-           <div className="form-group"><input type="Password" className="form-control" placeholder="Confirm Password"/></div>
-           
-           
-           <button  style={{textAlign:'center'}} type="submit" onClick={this.sendUserData} className="btn btn-primary"> Sign up </button>
-
-           <br></br>
-
-           <p className="signUp text-left ml-3"> Already have an account <Link exact to="login"><b> Login here </b></Link></p>
-
-            </form>
-            </div>
-            </div>
-            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </section>
-
-
-
-
-          
-          
             
         )
     }
